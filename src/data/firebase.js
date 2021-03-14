a0c98f95533d7999d4bd9a4a48adeece41decdf9
sidd sirius <sidd.benoit@gmail.com>
2021-03-14 14:32:35 -0400

    étapes B-I

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,7 +4,13 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyDg9Y9_KlsPAThZZTHRb88R3xXTC0U9o3Q",
+  authDomain: "pvt-h21.firebaseapp.com",
+  databaseURL: "https://pvt-h21-default-rtdb.firebaseio.com",
+  projectId: "pvt-h21",
+  storageBucket: "pvt-h21.appspot.com",
+  messagingSenderId: "424129836393",
+  appId: "1:424129836393:web:8e7d55521606a114a2f9f8"
 };
 
 // Initialiser Firebase
@@ -16,3 +22,4 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
