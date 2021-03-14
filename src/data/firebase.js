9b8905595a580362fb446e4253c0a542a797e33a
sidd sirius <sidd.benoit@gmail.com>
2021-03-14 14:39:09 -0400

    suppression du config firebase

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDg9Y9_KlsPAThZZTHRb88R3xXTC0U9o3Q",
-  authDomain: "pvt-h21.firebaseapp.com",
-  databaseURL: "https://pvt-h21-default-rtdb.firebaseio.com",
-  projectId: "pvt-h21",
-  storageBucket: "pvt-h21.appspot.com",
-  messagingSenderId: "424129836393",
-  appId: "1:424129836393:web:8e7d55521606a114a2f9f8"
+  
 };
 
 // Initialiser Firebase
